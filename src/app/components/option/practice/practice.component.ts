be951871e18df1af3afbec218c973148671eb9cc
trinhhoangyen <thienanh156@gmail.com>
2020-10-21 16:05:08 +0700

    progess bar, excuted

--- src/app/components/option/practice/practice.component.ts
+++ src/app/components/option/practice/practice.component.ts
@@ -8,21 +8,69 @@ import { ParagraphService } from 'src/app/services/paragraph.service';
   styleUrls: ['./practice.component.scss']
 })
 export class PracticeComponent implements OnInit {
-  list: any = [];
+  listPara: any = [];
   index: number = 0;
+
+  typeIndex: number = 0;
+  para = [];
+  paraLength: number = 0;
+  percentRight: number = 0;
+  status: boolean = false;
+  stringRight: string = '';
+  stringNow: string = '';
+  stringType: string = '';
+
+  backgroundColor: string = 'white';
+  color: string = 'blue';
+
   constructor(public optionSvc: OptionService, private paragraph: ParagraphService) { }
   ngOnInit(): void {
   this.paragraph.getList().then(res => {
     res.subscribe(d => {
-      this.list = d;
-      this.getParagraph()
+      this.listPara = d;
+      this.getParagraph();
+      this.typeNow();
     })
   });
   }
 
   getParagraph(){
-    let l = this.list as Array<string>;
+    let l = this.listPara as Array<string>;
     this.index = Math.floor(Math.random() * l.length);
+    this.stringType = this.listPara[this.index];
+    this.para = this.listPara[this.index].split(" ");
+    this.paraLength = this.para.length;;
   }
 
+  typerace(event: any){
+    if(event.target.value == this.para[this.typeIndex] || event.target.value == '')
+    {
+      this.color = 'blue';
+      this.backgroundColor = 'white';
+      return;
+    }
+    if(event.target.value.indexOf(' ') >= 0)
+      if(event.target.value == this.para[this.typeIndex] + ' '){
+        this.typeIndex++;
+
+        this.stringRight += event.target.value + ' ';
+        this.stringNow = this.para[this.typeIndex];
+        this.stringType = this.stringType.replace(this.stringNow, '');
+
+        this.percentRight = this.typeIndex/this.paraLength * 100;
+        event.target.value = '';
+        return;
+      }
+    
+    if(this.para[this.typeIndex].indexOf(event.target.value) < 0)
+    {
+      this.color = 'red';
+      this.backgroundColor = 'rgb(209, 87, 105)';
+      return;
+    }
+  }
+  typeNow(){
+    this.stringNow = this.para[this.typeIndex];
+    this.stringType = this.stringType.replace(this.stringNow, '');
+  }
 }
